b8c347548538fd4e8f630b1c69f1f2ecba51c9b3
JinkerLeong <jinker2003@gmail.com>
2021-12-29 12:51:48 +0800

    optimize load config

--- configModule/configManager.py
+++ configModule/configManager.py
@@ -1,17 +1,19 @@
 import json
 from configModule.myconfig import *
 from sys import stdout
+from helper import print
 
-globalConfig:config = None
+CONFIG_PATH = "/etc/AutoReconnectNetwork/config.json"
 
 def jsonToConfig(d):
     return config(d['startAfter'],d['waitToCheck'],d['targetHost'], d['pingCount'], d['usbDevices'])
 
-def loadConfig(configFile)->config:
-    with open(configFile) as f:
-        global globalConfig
-        globalConfig = jsonToConfig(json.load(f))
-        return globalConfig
+def loadConfig()->config:
+    print("load config from /etc/AutoReconnectNetwork/config.json")
+    with open(CONFIG_PATH) as f:
+        return jsonToConfig(json.load(f))
+
+globalConfig:config = loadConfig()
 
 def getConfig()->config:
     return globalConfig
@@ -23,5 +25,9 @@ def printConfig():
     print(f"Wait to check seconds: {globalConfig.waitToCheck}")
     print(f"Target host: {globalConfig.targetHost}")
     print(f"Ping count: {globalConfig.pingCount}")
+    num = 0
+    for device in globalConfig.usbDevices:
+        num = num + 1
+        print(f"Usb device#{num}: {device.name}, {device.ip}, {device.netmask}")
     print("=========================")
     stdout.flush()
